aed27c305fafaeb2bb11ba923fbd89a3d05e7763
Marc Suchard <msuchard@gmail.com>
2015-05-14 15:45:06 -0700

    Some CMake files

---
+++ CMakeModules/build_VexCL.cmake
@@ -0,0 +1,70 @@
+SET(VER 1.3.3)
+SET(URL https://github.com/ddemidov/vexcl/archive/${VER}.tar.gz)
+SET(MD5 bab6d4cc5fa8886ef23fb8548b81429a)
+
+SET(thirdPartyDir "${CMAKE_BINARY_DIR}/third_party")
+SET(srcDir "${thirdPartyDir}/vexcl-${VER}")
+SET(archive ${srcDir}.tar.gz)
+SET(inflated ${srcDir}-inflated)
+
+# the config to be used in the code
+SET(VexCL_INCLUDE_DIRS "${srcDir}/vexcl")
+
+# do we have to do it again?
+SET(doExtraction ON)
+IF(EXISTS "${inflated}")
+    FILE(READ "${inflated}" extractedMD5)
+    IF("${extractedMD5}" STREQUAL "${MD5}")
+        # nope, everything looks fine
+        return()
+    ENDIF()
+ENDIF()
+
+# lets get and extract boost compute
+
+MESSAGE(STATUS "VexXCL...")
+IF(EXISTS "${archive}")
+    FILE(MD5 "${archive}" md5)
+    IF(NOT "${md5}" STREQUAL "${MD5}")
+        MESSAGE("  wrong check sum ${md5}, redownloading")
+        FILE(REMOVE "${archive}")
+    ENDIF()
+ENDIF()
+
+IF(NOT EXISTS "${archive}")
+    MESSAGE(STATUS "  getting ${URL}")
+    FILE(DOWNLOAD "${URL}" ${archive}
+        STATUS rv
+        SHOW_PROGRESS)
+ENDIF()
+
+MESSAGE(STATUS "  validating ${archive}")
+FILE(MD5 "${archive}" md5)
+IF(NOT "${md5}" STREQUAL "${MD5}")
+    MESSAGE(WARNING "${archive}: Invalid check sum ${md5}. Expected was ${MD5}")
+    IF("${md5}" STREQUAL "d41d8cd98f00b204e9800998ecf8427e")
+        MESSAGE(STATUS "Trying wget ${URL}")
+        EXECUTE_PROCESS(COMMAND wget --no-check-certificate -O ${archive} ${URL})
+        FILE(MD5 "${archive}" md5_)
+        IF(NOT "${md5_}" STREQUAL "${MD5}")
+            MESSAGE(FATAL_ERROR "${archive}: Invalid check sum ${md5_}. Expected was ${MD5}")
+        ENDIF(NOT "${md5_}" STREQUAL "${MD5}")
+        MESSAGE(STATUS "wget successful")
+    ENDIF("${md5}" STREQUAL "d41d8cd98f00b204e9800998ecf8427e")
+ENDIF()
+
+IF(IS_DIRECTORY ${srcDir})
+    MESSAGE(STATUS "  cleaning ${cleaning}")
+    FILE(REMOVE_RECURSE ${srcDir})
+ENDIF()
+
+MESSAGE(STATUS "  extracting ${archive}")
+FILE(MAKE_DIRECTORY ${srcDir})
+EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E tar xfz ${archive}
+    WORKING_DIRECTORY ${thirdPartyDir}
+    RESULT_VARIABLE rv)
+IF(NOT rv EQUAL 0)
+    MESSAGE(FATAL_ERROR "'${archive}' extraction failed")
+ENDIF()
+
+FILE(WRITE ${inflated} "${MD5}")
---
+++ src/OpenCLMultiDimensionalScaling.hpp
@@ -0,0 +1,449 @@
+#ifndef _OPENCLMULTIDIMENSIONALSCALING_HPP
+#define _OPENCLMULTIDIMENSIONALSCALING_HPP
+
+#include "AbstractMultiDimensionalScaling.hpp"
+
+#define SSE
+
+namespace mds {
+
+template <typename RealType>
+class OpenCLMultiDimensionalScaling : public AbstractMultiDimensionalScaling {
+public:
+    OpenCLMultiDimensionalScaling(int embeddingDimension, int locationCount, long flags)
+        : AbstractMultiDimensionalScaling(embeddingDimension, locationCount, flags),
+          precision(0.0), storedPrecision(0.0),
+          oneOverSd(0.0), storedOneOverSd(0.0),
+          sumOfSquaredResiduals(0.0), storedSumOfSquaredResiduals(0.0),
+          sumOfTruncations(0.0), storedSumOfTruncations(0.0),
+
+          observations(locationCount * locationCount),
+
+          locations0(locationCount * embeddingDimension),
+		  locations1(locationCount * embeddingDimension),
+		  locationsPtr(&locations0),
+		  storedLocationsPtr(&locations1),
+
+          squaredResiduals(locationCount * locationCount),
+          storedSquaredResiduals(locationCount),
+
+          isStoredSquaredResidualsEmpty(false),
+          isStoredTruncationsEmpty(false)//,
+//          isStoredAllTruncationsEmpty(false)
+
+//           , nThreads(4) //, pool(nThreads)
+    {
+
+    	if (flags & Flags::LEFT_TRUNCATION) {
+    		isLeftTruncated = true;
+    		std::cout << "Using left truncation" << std::endl;
+
+    		truncations.resize(locationCount * locationCount);
+    		storedTruncations.resize(locationCount);
+    	}
+    }
+
+    virtual ~OpenCLMultiDimensionalScaling() { }
+
+    void updateLocations(int locationIndex, double* location, size_t length) {
+
+    	assert(length == embeddingDimension);
+
+    	if (updatedLocation != - 1) {
+    		// more than one location updated -- do a full recomputation
+    		residualsAndTruncationsKnown = false;
+    		isStoredSquaredResidualsEmpty = true;
+    		isStoredTruncationsEmpty = true;
+    	}
+
+    	updatedLocation = locationIndex;
+    	std::copy(location, location + length,
+    		begin(*locationsPtr) + locationIndex * embeddingDimension
+    	);
+
+    	sumsOfResidualsAndTruncationsKnown = false;
+    }
+
+    void computeResidualsAndTruncations() {
+
+		if (!residualsAndTruncationsKnown) {
+			if (isLeftTruncated) { // run-time dispatch to compile-time optimization
+				computeSumOfSquaredResiduals<true>();
+			} else {
+				computeSumOfSquaredResiduals<false>();
+			}
+			residualsAndTruncationsKnown = true;
+		} else {
+			if (isLeftTruncated) {
+				updateSumOfSquaredResidualsAndTruncations();
+			} else {
+				updateSumOfSquaredResiduals();
+			}
+		}
+    }
+
+    double getSumOfSquaredResiduals() {
+    	if (!sumsOfResidualsAndTruncationsKnown) {
+			computeResidualsAndTruncations();
+			sumsOfResidualsAndTruncationsKnown = true;
+		}
+		return sumOfSquaredResiduals;
+ 	}
+
+ 	double getSumOfLogTruncations() {
+    	if (!sumsOfResidualsAndTruncationsKnown) {
+			computeResidualsAndTruncations();
+			sumsOfResidualsAndTruncationsKnown = true;
+		}
+ 		return sumOfTruncations;
+ 	}
+
+    void storeState() {
+    	storedSumOfSquaredResiduals = sumOfSquaredResiduals;
+
+    	std::copy(begin(*locationsPtr), end(*locationsPtr),
+    		begin(*storedLocationsPtr));
+
+    	isStoredSquaredResidualsEmpty = true;
+
+    	updatedLocation = -1;
+
+    	storedPrecision = precision;
+    	storedOneOverSd = oneOverSd;
+
+    	// Handle truncation
+    	if (isLeftTruncated) {
+    		storedSumOfTruncations = sumOfTruncations;
+			isStoredTruncationsEmpty = true;
+    	}
+    }
+
+    void restoreState() {
+    	sumOfSquaredResiduals = storedSumOfSquaredResiduals;
+    	sumsOfResidualsAndTruncationsKnown = true;
+
+		if (!isStoredSquaredResidualsEmpty) {
+    		std::copy(
+    			begin(storedSquaredResiduals),
+    			end(storedSquaredResiduals),
+    			begin(squaredResiduals) + updatedLocation * locationCount
+    		);
+    		for (int j = 0; j < locationCount; ++j) {
+    			squaredResiduals[j * locationCount + updatedLocation]
+    				= storedSquaredResiduals[j];
+    		}
+    		residualsAndTruncationsKnown = true;
+    	} else {
+    		residualsAndTruncationsKnown = false; // Force recompute;  TODO cache
+    	}
+
+    	// Handle truncation
+    	if (isLeftTruncated) {
+	    	sumOfTruncations = storedSumOfTruncations;
+
+	    	if (!isStoredTruncationsEmpty) {
+	    		std::copy(
+	    			begin(storedTruncations),
+	    			end(storedTruncations),
+	    			begin(truncations) + updatedLocation * locationCount
+	    		);
+	    		for (int j = 0; j < locationCount; ++j) {
+	    			truncations[j * locationCount + updatedLocation]
+	    				= storedTruncations[j];
+	    		}
+	    	}
+	    }
+
+    	precision = storedPrecision;
+    	oneOverSd = storedOneOverSd;
+
+    	auto tmp1 = storedLocationsPtr;
+    	storedLocationsPtr = locationsPtr;
+    	locationsPtr = tmp1;
+    }
+
+    void setPairwiseData(double* data, size_t length) {
+		assert(length == observations.size());
+		std::copy(data, data + length, begin(observations));
+    }
+
+    void setParameters(double* data, size_t length) {
+		assert(length == 1); // Call only with precision
+		precision = data[0]; // TODO Remove
+		oneOverSd = std::sqrt(data[0]);
+
+		// Handle truncations
+		if (isLeftTruncated) {
+			residualsAndTruncationsKnown = false;
+			sumsOfResidualsAndTruncationsKnown = false;
+
+    		isStoredSquaredResidualsEmpty = true;
+    		isStoredTruncationsEmpty = true;
+
+		}
+    }
+
+    void makeDirty() {
+    	sumsOfResidualsAndTruncationsKnown = false;
+    	residualsAndTruncationsKnown = false;
+    }
+
+
+	int count = 0;
+
+	template <bool withTruncation>
+	void computeSumOfSquaredResiduals() {
+
+		RealType lSumOfSquaredResiduals = 0.0;
+		RealType lSumOfTruncations = 0.0;
+
+		for (int i = 0; i < locationCount; ++i) { // TODO Parallelize
+			for (int j = 0; j < locationCount; ++j) { // TODO j = i + 1 ???
+
+				const auto distance = calculateDistance<mm::GPUMemoryManager<RealType>>(
+					begin(*locationsPtr) + i * embeddingDimension,
+					begin(*locationsPtr) + j * embeddingDimension,
+					embeddingDimension
+				);
+				const auto residual = distance - observations[i * locationCount + j];
+				const auto squaredResidual = residual * residual;
+				squaredResiduals[i * locationCount + j] = squaredResidual;
+// 				squaredResiduals[j * locationCount + i] = squaredResidual;
+				lSumOfSquaredResiduals += squaredResidual;
+
+				if (withTruncation) { // compile-time check
+					const auto truncation = (i == j) ? RealType(0) :
+						math::logCdf(std::fabs(residual) * oneOverSd);
+					truncations[i * locationCount + j] = truncation;
+// 					truncations[j * locationCount + i] = truncation;
+					lSumOfTruncations += truncation;
+				}
+			}
+		}
+
+    	lSumOfSquaredResiduals /= 2.0;
+    	sumOfSquaredResiduals = lSumOfSquaredResiduals;
+
+    	if (withTruncation) {
+    		lSumOfTruncations /= 2.0;
+    		sumOfTruncations = lSumOfTruncations;
+    	}
+
+	    residualsAndTruncationsKnown = true;
+	    sumsOfResidualsAndTruncationsKnown = true;
+	}
+
+	void updateSumOfSquaredResiduals() {
+		// double delta = 0.0;
+
+		const int i = updatedLocation;
+		isStoredSquaredResidualsEmpty = false;
+
+		auto start  = begin(*locationsPtr) + i * embeddingDimension;
+		auto offset = begin(*locationsPtr);
+
+		RealType delta =
+
+// 		accumulate_thread(0, locationCount, double(0),
+ 		accumulate(0, locationCount, RealType(0),
+// 		accumulate_tbb(0, locationCount, double(0),
+
+			[this, i, &offset,
+			&start](const int j) {
+                const auto distance = calculateDistance<mm::GPUMemoryManager<RealType>>(
+                    start,
+                    offset + embeddingDimension * j,
+                    embeddingDimension
+                );
+
+                const auto residual = distance - observations[i * locationCount + j];
+                const auto squaredResidual = residual * residual;
+
+            	// store old value
+            	const auto oldSquaredResidual = squaredResiduals[i * locationCount + j];
+            	storedSquaredResiduals[j] = oldSquaredResidual;
+
+                const auto inc = squaredResidual - oldSquaredResidual;
+
+            	// store new value
+                squaredResiduals[i * locationCount + j] = squaredResidual;
+                squaredResiduals[j * locationCount + i] = squaredResidual;
+
+                return inc;
+            }
+		);
+
+		sumOfSquaredResiduals += delta;
+	}
+
+// 	int count = 0
+	int count2 = 0;
+
+
+// 	void updateTruncations() {
+//
+// 		const int i = updatedLocation;
+//
+// 		isStoredTruncationsEmpty = false;
+//
+// 		auto start  = begin(*locationsPtr) + i * embeddingDimension;
+// 		auto offset = begin(*locationsPtr);
+//
+// 		RealType delta =
+// // 		accumulate_thread(0, locationCount, double(0),
+//  		accumulate(0, locationCount, RealType(0),
+// // 		accumulate_tbb(0, locationCount, double(0),
+//
+// 			[this, i, &offset, //oneOverSd,
+// 			&start](const int j) {
+//
+//                 const auto squaredResidual = squaredResiduals[i * locationCount + j];
+//
+//                 const auto truncation = (i == j) ? RealType(0) :
+//                 	math::logCdf(std::sqrt(squaredResidual) * oneOverSd);
+//
+//                 const auto oldTruncation = truncations[i * locationCount + j];
+//                 storedTruncations[j] = oldTruncation;
+//
+//                 const auto inc = truncation - oldTruncation;
+//
+//                 truncations[i * locationCount + j] = truncation;
+//                 truncations[j * locationCount + i] = truncation;
+//
+//                 return inc;
+//             }
+// 		);
+//
+//  		sumOfTruncations += delta;
+// 	}
+
+	void updateSumOfSquaredResidualsAndTruncations() {
+
+		const int i = updatedLocation;
+		isStoredSquaredResidualsEmpty = false;
+		isStoredTruncationsEmpty = false;
+
+		auto start  = begin(*locationsPtr) + i * embeddingDimension;
+		auto offset = begin(*locationsPtr);
+
+		std::complex<RealType> delta =
+
+ 		accumulate(0, locationCount, std::complex<RealType>(RealType(0), RealType(0)),
+
+			[this, i, &offset, //oneOverSd,
+			&start](const int j) {
+                const auto distance = calculateDistance<mm::GPUMemoryManager<RealType>>(
+                    start,
+                    offset + embeddingDimension * j,
+                    embeddingDimension
+                );
+
+                const auto residual = distance - observations[i * locationCount + j];
+                const auto squaredResidual = residual * residual;
+
+            	// store old value
+            	const auto oldSquaredResidual = squaredResiduals[i * locationCount + j];
+            	storedSquaredResiduals[j] = oldSquaredResidual;
+
+                const auto inc = squaredResidual - oldSquaredResidual;
+
+            	// store new value
+                squaredResiduals[i * locationCount + j] = squaredResidual;
+                squaredResiduals[j * locationCount + i] = squaredResidual;
+
+                const auto truncation = (i == j) ? RealType(0) :
+                	math::logCdf(std::fabs(residual) * oneOverSd);
+
+                const auto oldTruncation = truncations[i * locationCount + j];
+                storedTruncations[j] = oldTruncation;
+
+                const auto inc2 = truncation - oldTruncation;
+
+                truncations[i * locationCount + j] = truncation;
+                truncations[j * locationCount + i] = truncation;
+
+                return std::complex<RealType>(inc, inc2);
+            }
+		);
+
+		sumOfSquaredResiduals += delta.real();
+ 		sumOfTruncations += delta.imag();
+	}
+
+#ifdef SSE
+    template <typename VectorType, typename Iterator>
+    double calculateDistance(Iterator iX, Iterator iY, int length) const {
+        auto sum = static_cast<double>(0);
+
+        using AlignedValueType = typename VectorType::allocator_type::aligned_value_type;
+
+        AlignedValueType* x = &*iX;
+        AlignedValueType* y = &*iY;
+
+        for (int i = 0; i < 2; ++i, ++x, ++y) {
+            const auto difference = *x - *y;
+            sum += difference * difference;
+        }
+        return std::sqrt(sum);
+    }
+#else // SSE
+    template <typename VectorType, typename Iterator>
+    double calculateDistance(Iterator x, Iterator y, int length) const {
+        auto sum = static_cast<double>(0);
+
+        for (int i = 0; i < 2; ++i, ++x, ++y) {
+            const auto difference = *x - *y;
+            sum += difference * difference;
+        }
+        return std::sqrt(sum);
+    }
+#endif // SSE
+
+	template <typename Integer, typename Function, typename Real>
+	inline Real accumulate(Integer begin, Integer end, Real sum, Function function) {
+		for (; begin != end; ++begin) {
+			sum += function(begin);
+		}
+		return sum;
+	}
+
+private:
+	double precision;
+	double storedPrecision;
+
+	double oneOverSd;
+	double storedOneOverSd;
+
+    double sumOfSquaredResiduals;
+    double storedSumOfSquaredResiduals;
+
+    double sumOfTruncations;
+    double storedSumOfTruncations;
+
+    mm::GPUMemoryManager<RealType> observations;
+
+    mm::GPUMemoryManager<RealType> locations0;
+    mm::GPUMemoryManager<RealType> locations1;
+
+    mm::GPUMemoryManager<RealType>* locationsPtr;
+    mm::GPUMemoryManager<RealType>* storedLocationsPtr;
+
+    mm::GPUMemoryManager<RealType> squaredResiduals;
+    mm::GPUMemoryManager<RealType> storedSquaredResiduals;
+
+    mm::GPUMemoryManager<RealType> truncations;
+    mm::GPUMemoryManager<RealType> storedTruncations;
+
+    bool isStoredSquaredResidualsEmpty;
+    bool isStoredTruncationsEmpty;
+
+//     bool isStoredAllTruncationsEmpty;
+
+//     int nThreads;
+//     ThreadPool pool;
+
+};
+
+} // namespace mds
+
+#endif // _OPENCLMULTIDIMENSIONALSCALING_HPP
