69c65dd0c18ea9aa2c51bbbf947ad0ae241096e2
Marc Suchard <msuchard@gmail.com>
2015-09-24 13:53:50 -0700

    Use CMake to find TBB

---
+++ CMakeModules/build_tbb.cmake
@@ -0,0 +1,73 @@
+SET(VER 44_20150728oss)
+SET(URL https://www.threadingbuildingblocks.org/sites/default/files/software_releases/source/tbb${VER}_src.tgz)
+SET(MD5 d41d8cd98ddf00b204e9800998ecf8427e)
+
+SET(thirdPartyDir "${CMAKE_BINARY_DIR}/third_party")
+SET(srcDir "${thirdPartyDir}/tbb${VER}")
+SET(archive ${srcDir}_src.tgz)
+SET(inflated ${srcDir}-inflated)
+
+# the config to be used in the code
+SET(TBB_INCLUDE_DIRS "${srcDir}/include")
+
+# do we have to do it again?
+SET(doExtraction ON)
+IF(EXISTS "${inflated}")
+    FILE(READ "${inflated}" extractedMD5)
+    IF("${extractedMD5}" STREQUAL "${MD5}")
+        # nope, everything looks fine
+        return()
+    ENDIF()
+ENDIF()
+
+# lets get and extract boost compute
+
+MESSAGE(STATUS "TBB...")
+IF(EXISTS "${archive}")
+    FILE(MD5 "${archive}" md5)
+    IF(NOT "${md5}" STREQUAL "${MD5}")
+        MESSAGE("  wrong check sum ${md5}, redownloading")
+        FILE(REMOVE "${archive}")
+    ENDIF()
+ENDIF()
+
+IF(NOT EXISTS "${archive}")
+    MESSAGE(STATUS "  getting ${URL}")
+    FILE(DOWNLOAD "${URL}" ${archive}
+        STATUS rv
+        SHOW_PROGRESS)
+ENDIF()
+
+MESSAGE(STATUS "  validating ${archive}")
+FILE(MD5 "${archive}" md5)
+IF(NOT "${md5}" STREQUAL "${MD5}")
+    MESSAGE(WARNING "${archive}: Invalid check sum ${md5}. Expected was ${MD5}")
+    IF("${md5}" STREQUAL "d41d8cd98f00b204e9800998ecf8427e")
+        MESSAGE(STATUS "Trying wget ${URL}")
+        EXECUTE_PROCESS(COMMAND wget --no-check-certificate -O ${archive} ${URL})
+        FILE(MD5 "${archive}" md5_)
+        IF(NOT "${md5_}" STREQUAL "${MD5}")
+            MESSAGE(FATAL_ERROR "${archive}: Invalid check sum ${md5_}. Expected was ${MD5}")
+        ENDIF(NOT "${md5_}" STREQUAL "${MD5}")
+        MESSAGE(STATUS "wget successful")
+    ENDIF("${md5}" STREQUAL "d41d8cd98f00b204e9800998ecf8427e")
+ENDIF()
+
+IF(IS_DIRECTORY ${srcDir})
+    MESSAGE(STATUS "  cleaning ${cleaning}")
+    FILE(REMOVE_RECURSE ${srcDir})
+ENDIF()
+
+MESSAGE(STATUS "  extracting ${archive}")
+FILE(MAKE_DIRECTORY ${srcDir})
+EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E tar xfz ${archive}
+    WORKING_DIRECTORY ${thirdPartyDir}
+    RESULT_VARIABLE rv)
+IF(NOT rv EQUAL 0)
+    MESSAGE(FATAL_ERROR "'${archive}' extraction failed")
+ENDIF()
+
+# Build TBB
+
+
+FILE(WRITE ${inflated} "${MD5}")
