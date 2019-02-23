c0937da05c1c90f91be08a844d6114a13736593a
Andrew Holbrook <homefolder@Andrews-MacBook-Pro.local>
2019-02-23 11:06:15 -0800

    Revert "please work now, O tbb"
    This reverts commit 90a50f9b92d1dc0c6570128bba28c49dc09803fc.

--- src/NewMultiDimensionalScaling.hpp
+++ src/NewMultiDimensionalScaling.hpp
@@ -102,8 +102,7 @@ public:
     			threads = tbb::task_scheduler_init::default_num_threads();
     		}
             std::cout << "Using " << threads << " threads" << std::endl;
-			std::shared_ptr<tbb::task_scheduler_init> task{nullptr};
-			task = std::make_shared<tbb::task_scheduler_init>(threads);
+			std::make_shared<tbb::task_scheduler_init>(threads);
     	}
     }
 
